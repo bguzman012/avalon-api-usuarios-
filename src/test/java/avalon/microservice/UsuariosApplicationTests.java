dd938eb13b17092e85f2bf2284169fd0e753fa6d
Bryam Guzman <bryamgmfn@gmail.com>
2024-05-19 23:11:49 -0500

    AVALON

---
+++ src/test/java/avalon/microservice/UsuariosApplicationTests.java
@@ -0,0 +1,14 @@
+package avalon.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import avalon.usuarios.MicroserviceUsuariosApplication;
+
+@SpringBootTest(classes = MicroserviceUsuariosApplication.class)
+public class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
